884c463522ca45ee91d88575ac5578c6273ab34e
Sebastian <sebastian.stoelen@gmail.com>
2019-05-15 10:40:25 +0200

    🌈🦄🌈🦄🌈

---
+++ src/main/java/kata/BowlingGame.java
@@ -0,0 +1,14 @@
+package kata;
+
+public class BowlingGame {
+
+    public void roll(int pins) {
+        //TODO: this method should register the pins a player trhows down
+    }
+
+    public int score() {
+        //TODO: this method should return the score of the bowling game
+        throw new RuntimeException("Not impelemented");
+    }
+
+}
---
+++ src/test/java/kata/BowlingGameTest.java
@@ -0,0 +1,19 @@
+package kata;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
+    @Test
+    public void thisIsADummyTestMethod() {
+
+    }
+}
